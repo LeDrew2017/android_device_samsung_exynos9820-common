b8828936202ed5201c1b46a57e9897550303c0ed
LeDrew2017 <33235594+LeDrew2017@users.noreply.github.com>
2023-08-10 09:37:43 -0400

    exynos9820-common: remove unused gapps flag
    - set PRODUCT_BROKEN_VERIFY_USES_LIBRARIES := true
    - set PRODUCT_USE_SCUDO += true

--- common.mk
+++ common.mk
@@ -332,6 +332,5 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
 
-# GApps
-TARGET_IS_GROUPER := true
-include vendor/gapps/arm64/arm64-vendor.mk
+PRODUCT_USE_SCUDO += true
+PRODUCT_BROKEN_VERIFY_USES_LIBRARIES := true
